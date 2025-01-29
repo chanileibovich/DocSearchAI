bdc6dd28923e6af6b698bcbebda22aee9e63f6d8
chanileibovich <c0548559199@gmail.com>
2025-01-29 22:29:49 +0200

    upload project

---
+++ files/fronted.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+from files.backend import stream_rag_chain
+
+
+if __name__ == "__main__":
+    with gr.Blocks(fill_height=True) as demo:
+        gr.ChatInterface(
+            stream_rag_chain,
+            type="messages",
+            multimodal=False,
+            theme="soft"
+        )
+    demo.launch(share=False)
